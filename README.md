a1fa9e9430f4ff3c0c185a8ffcc005d47efca4dd
George-Doge <juraj.jencar@gmail.com>
2024-12-09 11:49:47 +0100

    arduino IDE branch of code

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is repo to store code for Raspberry Pi Pico WH in our CanSat project
 
 [Raspberry Pi Pico W Pinout](https://picow.pinout.xyz/)
+
+## Used libraries
+- [Adafruit BME280](https://github.com/adafruit/Adafruit_BME280_Library)
+- [Adafruit Unified Sensor](https://github.com/adafruit/Adafruit_Sensor)
---
+++ main.ino
@@ -0,0 +1,66 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+Adafruit_BME280 bme; // I2C (default pins for Raspberry Pi Pico: GPIO 4 (SDA), GPIO 5(SCL)
+
+unsigned long delayTime;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("IJFAOIAJFPOIJAFS");
+  
+  Wire.setSDA(8);
+  Wire.setSCL(9);
+  Wire.begin();
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println("BME280 with Raspberry Pi Pico");
+
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  delayTime = 1000;
+
+  Serial.println();
+}
+
+void loop() { 
+  printValues();
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delayTime);
+}
+
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
+}
